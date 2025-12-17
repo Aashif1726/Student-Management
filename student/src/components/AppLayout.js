4d2778979791d188acdb67f0f1f265c2a7aae8cf
Aashif <aashifjamaan1726@gmail.com>
2025-12-17 04:08:52 -0500

    Notifications and Settings Added

--- student/src/components/AppLayout.js
+++ student/src/components/AppLayout.js
@@ -4,6 +4,10 @@ import { Outlet } from "react-router-dom";
 import SideBar from "./SideBar";
 import "./AppLayout.css";
 import Dashboard from "./Dashboard";
+import SettingsIcon from '@mui/icons-material/Settings';
+import { Badge, Button, Tooltip } from "@mui/material";
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ChatBot from "react-chatbotify"
 
 const AppLayout = () => {
   return (
@@ -11,7 +15,12 @@ const AppLayout = () => {
       <SideBar />
       <main className="app-content">
         <Outlet/>
-       
+        <div style={{float:"right"}}>
+           <Button style={{color:"#4040a1"}}><Tooltip title="notifications"><Badge badgeContent={1} color="error" variant="dot"> <NotificationsIcon/></Badge></Tooltip></Button>
+          <Button style={{color:"#4040a1"}}><Tooltip title="settings"><SettingsIcon/></Tooltip></Button>
+           
+       </div>
+       {/* <ChatBot/> */}
       </main>
     </div>
   );
