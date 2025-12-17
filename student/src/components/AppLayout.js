1fd63d79ca2d135e7c15799853140bf69990a650
Aashif <aashifjamaan1726@gmail.com>
2025-12-17 05:27:28 -0500

    Speed Dial Created

--- student/src/components/AppLayout.js
+++ student/src/components/AppLayout.js
@@ -5,12 +5,16 @@ import SideBar from "./SideBar";
 import "./AppLayout.css";
 import Dashboard from "./Dashboard";
 import SettingsIcon from '@mui/icons-material/Settings';
-import { Badge, Button, Checkbox, FormControlLabel, Switch, Tooltip } from "@mui/material";
+import { Badge, Button, Checkbox, FormControlLabel, SpeedDial, SpeedDialAction, SpeedDialIcon, Switch, Tooltip } from "@mui/material";
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import ChatBot from "react-chatbotify"
+import MessageIcon from '@mui/icons-material/Message';
 
 
 const AppLayout = () => {
+  const hanldeMessage =()=>{
+    window.location.href="mailto:aashif2606@gmail.com"
+  }
   return (
     <div className="app-layout">
       <SideBar />
@@ -23,7 +27,12 @@ const AppLayout = () => {
        </div>
        {/* <ChatBot/> */}
       </main>
-
+          <SpeedDial ariaLabel="speed dial" 
+          sx={{position:"absolute",bottom:16,right:16}} 
+          icon={<SpeedDialIcon/>}>
+            <SpeedDialAction icon={<MessageIcon/>} tooltipTitle="Message"  onClick={hanldeMessage}/>
+          </SpeedDial>
+     
 
     </div>
   );
