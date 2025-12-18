7186fb48cbd814d2823e9d8eedaea4c1b6982510
Aashif <aashifjamaan1726@gmail.com>
2025-12-18 11:59:24 -0500

    Students Data Created

--- student/src/components/Students.js
+++ student/src/components/Students.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Tooltip } from '@mui/material';
 import InfoOutlineRoundedIcon from '@mui/icons-material/InfoOutlineRounded';
+import axios from "axios";
+import {Outlet} from "react-router-dom"
+import "./Students.css"
 
 function Students() {
+    const [students,setStudents]=useState([])
+
+    useEffect(()=>{
+        axios.get("http://localhost:8080/stud")
+        .then(res=>{
+            console.log(res.data)
+            setStudents(res.data.response || [])
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    },[])
     return (
         <>
             <Tooltip title="This page shows Students details"><InfoOutlineRoundedIcon /></Tooltip>  <span style={{padding:"4px"}}><b>Students Page</b></span>
+            <div style={{width:"100%"}}><br/><br/>
+             <table>
+                <tbody>
+                     <tr>
+                    <th>Id</th>
+                     <th>Student Name</th>
+                      <th>Email</th>
+                       <th>Branch</th>
+                        <th>Place</th>
+                         <th>Marks</th>
+                          <th>Gender</th>
+                </tr>
+                {
+                    students.map((student)=>(
+                        <tr key={student.id}>
+                            <td>{student.id}</td>
+                            <td>{student.name}</td>
+                            <td>{student.email}</td>
+                            <td>{student.branch}</td>
+                            <td>{student.place}</td>
+                            <td>{student.marks}</td>
+                            <td>{student.gender}</td>
+                        </tr>
+                    ))
+                }
+
+                </tbody>
+               
+                
+             </table>
+             </div>
+
         </>
     )
 }
