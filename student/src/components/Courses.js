4cc9d4e256fb895aea78ec69dcec0da4dff0d661
Aashif <aashifjamaan1726@gmail.com>
2025-12-24 05:38:16 -0500

    Style Added

--- student/src/components/Courses.js
+++ student/src/components/Courses.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import InfoOutlineRoundedIcon from "@mui/icons-material/InfoOutlineRounded";
 import { Tooltip } from "@mui/material";
+import "./Stats.css"
 
 function Courses() {
   const [stats, setStats] = useState([]);
